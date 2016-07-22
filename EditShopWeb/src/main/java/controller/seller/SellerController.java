11464dc4579f2779e6cbfd1bf56d3025528aafb3
CD <CD@Windy>
2016-07-22 16:00:19 +0900

    sellercontroller

--- EditShopWeb/src/main/java/controller/seller/SellerController.java
+++ EditShopWeb/src/main/java/controller/seller/SellerController.java
@@ -2,18 +2,32 @@ package controller.seller;
 
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+import shop.cls.order.service.OrderService;
+import shop.cls.product.service.ProductService;
+import shop.cls.seller.service.SellerAddService;
 import vo.Product;
 
 @Controller
 public class SellerController {
 
+	@Autowired
+	private ProductService productService;
+	@Autowired
+	private OrderService orderService;
+	@Autowired
+	private SellerAddService sellerAddService;
+	
 	@RequestMapping(value="seller.do")
-	public String loginSeller(HttpSession session){
+	public ModelAndView loginSeller(HttpSession session){
 		
-		return "";
+		ModelAndView mnv = new ModelAndView();
+		
+		return mnv;
 	}
 	
 	public boolean registSeller(){
